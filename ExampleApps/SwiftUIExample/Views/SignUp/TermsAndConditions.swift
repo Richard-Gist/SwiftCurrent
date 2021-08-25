4cfd5ed10eae8c15d9d3bf00da1b5bce338e1a18
Richard Gist <Richard.Gist@wwt.com>
2021-08-25 13:23:38 -0600

    [skip ci] video-creator - starting tos - RAG

---
+++ ExampleApps/SwiftUIExample/Views/SignUp/TermsAndConditions.swift
@@ -0,0 +1,21 @@
+//
+//  TermsAndConditions.swift
+//  TermsAndConditions
+//
+//  Created by Richard Gist on 8/25/21.
+//  Copyright © 2021 WWT and Tyler Thompson. All rights reserved.
+//
+
+import SwiftUI
+
+struct TermsAndConditions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TermsAndConditions_Previews: PreviewProvider {
+    static var previews: some View {
+        TermsAndConditions()
+    }
+}
