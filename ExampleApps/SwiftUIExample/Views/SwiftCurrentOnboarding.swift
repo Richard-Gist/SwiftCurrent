10f7265c0c93494b16390d2cc743dbf4350bdf5f
Tyler Thompson <tyler.thompson@wwt.com>
2021-08-26 13:08:16 -0600

    [skip ci] video-creator - Added asset for social media logo - TT RAG
    Co-authored-by: Richard Gist <Richard.Gist@wwt.com>

---
+++ ExampleApps/SwiftUIExample/Views/SwiftCurrentOnboarding.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftCurrentOnboarding.swift
+//  SwiftUIExample
+//
+//  Created by Tyler Thompson on 8/26/21.
+//  Copyright © 2021 WWT and Tyler Thompson. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftCurrentOnboarding: View {
+    var body: some View {
+        Text("Something eye catching")
+    }
+}
+
+struct SwiftCurrentOnboarding_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftCurrentOnboarding()
+            .preferredColorScheme(.dark)
+    }
+}
